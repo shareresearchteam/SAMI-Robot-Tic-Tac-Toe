f961ffdd8ad2c0fe2fb658e8450f0c55a5362e18
jlruballos <124950155+jlruballos@users.noreply.github.com>
2024-06-13 17:27:08 -0700

    Update Communication.h

--- Arduino/JamieSensors/Communication.h
+++ Arduino/JamieSensors/Communication.h
@@ -11,29 +11,29 @@ void sendUltrasonicData(float distance) {
   Serial.print(startMarker);
   Serial.print(ultraMsg); // Message type for Ultrasonic
   Serial.print(distance);
-  Serial.println(endMarker);
+  Serial.write(endMarker);
 }
 
 void sendPirData(int motionStatus) {
   if (motionStatus != -1) { // Only send data on state change
-    Serial.print(startMarker);
-    Serial.print(pirMsg); // Message type for PIR
-    Serial.print(motionStatus);
+    Serial.write(startMarker);
+    Serial.write(pirMsg); // Message type for PIR
+    Serial.write(motionStatus);
     Serial.println(endMarker);
   }
 }
 
 void sendPushButtonData1(int buttonState1) {
-    Serial.print(startMarker);
-    Serial.print(pbMsg1); // Message type for push button 1
-    Serial.print(buttonState1);
+    Serial.write(startMarker);
+    Serial.write(pbMsg1); // Message type for push button 1
+    Serial.write(buttonState1);
     Serial.println(endMarker);
   }
 
 void sendPushButtonData2(int buttonState2) {
-    Serial.print(startMarker);
-    Serial.print(pbMsg2); // Message type for push button 2
-    Serial.print(buttonState2);
+    Serial.write(startMarker);
+    Serial.write(pbMsg2); // Message type for push button 2
+    Serial.write(buttonState2);
     Serial.println(endMarker);
   }
   
