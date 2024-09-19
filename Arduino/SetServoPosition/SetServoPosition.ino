1aa86693d71935f931a0cdfe72b330271a79557e
Reakain <reakain@users.noreply.github.com>
2024-09-19 16:53:22 -0700

    setup the set servo to set more than one motor

--- Arduino/SetServoPosition/SetServoPosition.ino
+++ Arduino/SetServoPosition/SetServoPosition.ino
@@ -1,10 +1,11 @@
 #include "include.h"
 #include "SerialServo.h"
 
+int numMotors = 4;
 // Update the ID to match the specific servo you want to move
-int motorID = 14;
+int motorIDs[] = {4,5,6,7};
 // Set the position in degrees (from 0 to 240) that you want the servo to move to
-int motorPosition = 90;
+int motorPositions[] = {125,180,125,90};
 
 void setup() {
   // begin serial communication with the pc
@@ -14,16 +15,19 @@ void setup() {
   // Give it some initialization time
   delay(1000);
 
-  // Set the motorPosition for motorID mapping to the inbuilt positioning ticks,
-  // and get there in 1000 ms
-  LobotSerialServoMove(Serial1, motorID, map(motorPosition,0,240,0,1000), 1000);
+  for(int i = 0; i < numMotors; i++) {
+    // Set the motorPosition for motorID mapping to the inbuilt positioning ticks,
+    // and get there in 1000 ms
+    LobotSerialServoMove(Serial1, motorIDs[i], map(motorPositions[i],0,240,0,1000), 1000);
+    // Then tell us on the serial monitor on the pc that it completed the movement!
+    Serial.print("Motor ID, ");
+    Serial.print(motorIDs[i]);
+    Serial.print(", set to angle, ");
+    Serial.println(motorPositions[i]);
+  }
   // Then wait the 1000 ms for it to complete
   delay(1000);
-  // Then tell us on the serial monitor on the pc that it completed the movement!
-  Serial.print("Motor ID, ");
-  Serial.print(motorID);
-  Serial.print(", set to angle, ");
-  Serial.println(motorPosition);
+  
 }
 
 void loop() {
