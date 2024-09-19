6f3869c067b23f4809d417ec08c9c445b7d71ef3
jlruballos <124950155+jlruballos@users.noreply.github.com>
2024-09-19 16:39:56 -0700

    Update globalVariables.h

--- Arduino/JamieSensors/globalVariables.h
+++ Arduino/JamieSensors/globalVariables.h
@@ -1,10 +1,14 @@
 // Trigger Pin of Ultrasonic Sensor
-#define PING_PIN 13
+#define TRIG_PIN 7
 // Echo Pin of Ultrasonic Sensor
-#define ECHO_PIN 12
+#define ECHO_PIN 8
+
+#define BUZZER_PIN 5
 
 //PIR sensor input pin
 #define PIR_PIN  10
+#define PIR_PINR  11
+#define PIR_PINL  12
 
 //Push button input pin
 #define PUSHB_1 4
@@ -12,3 +16,10 @@
 
 #define NUM_READINGS 15  // Number of readings to average.
 #define THRESHOLD    50   // Deviation threshold from the average (in centimeters).
+
+
+// Constants
+const unsigned int MAX_DIST = 23200;
+const unsigned long ALARM_DURATION_MS = 1000;
+const unsigned long DETECTION_PERIOD_MS = 5000; // 5 seconds
+const unsigned long ULTRASONIC_INTERVAL_MS = 100; // Interval between ultrasonic measurements
