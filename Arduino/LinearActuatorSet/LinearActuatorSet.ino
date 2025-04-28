ecd08a1f182161a07afd968001357a2490443226
la-rup <52813297+la-rup@users.noreply.github.com>
2025-04-28 13:13:09 -0700

    new updated code

--- Arduino/LinearActuatorSet/LinearActuatorSet.ino
+++ Arduino/LinearActuatorSet/LinearActuatorSet.ino
@@ -8,11 +8,23 @@
 
 Servo myservo;  // create servo object to control a servo
 
-int val = 20;   // variable to set linear actuator: range = 0 - 20
+int position = 0;   // variable to set linear actuator: range = 0 - 20
 
 void setup() {
+  pinMode(33, OUTPUT); //torso neck relay
+  pinMode(26, OUTPUT); //leg relay
+  pinMode(28, OUTPUT); //arm relay
+  digitalWrite(28, true);
+  digitalWrite(33, true);
+  digitalWrite(26, true);
+  delay(1000);
   myservo.attach(34);  // attaches the servo on pin to the servo object
-  val = map(val, 0, 1023, 0, 20);
-  myservo.write(val);
+  delay(1000);
+  position = map(position, 0, 1023, 0, 20);
+  myservo.write(1000);
+  
 }
 
+void loop() {
+  
+}
