b1754b0180fe34afe9db5382498cbf981ec4e950
Antara Shah <antara.a.shah@gmail.com>
2025-06-30 16:44:41 -0700

    angle clamping added so now only goes to min or max

--- software/SAMI_UI.py
+++ software/SAMI_UI.py
@@ -7,7 +7,7 @@ from SAMIControl import SAMIControl
 
 class SAMIControlUI(SAMIControl, QWidget):
     def __init__(self, 
-                 arduino_port='COM8', 
+                 arduino_port='/dev/ttyUSB0', 
                  baud_rate=115200,
                  joint_config_file='Joint_config.json',
                  behavior_folder='behaviors',
